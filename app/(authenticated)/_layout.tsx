e0ef0a2a8410305cfb42354c884f470bb3280f02
Nguyễn Đình Tuấn Anh <ngdtuananh01@gmail.com>
2025-02-06 00:38:46 +0700

    feature: add update popup

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -5,82 +5,87 @@ import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useTranslation } from "react-i18next";
+import UpdatePopup from "@/components/popup/UpdatePopup";
+import { Fragment } from "react";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const { t } = useTranslation();
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        headerStyle: {
-          backgroundColor: "black",
-        },
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: "absolute",
-          },
-          default: {
+    <Fragment>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {
             backgroundColor: "black",
           },
-        }),
-      }}
-    >
-      <Tabs.Screen
-        name="(store)"
-        options={{
-          title: t("store"),
-          popToTopOnBlur: true,
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="storefront" size={28} color={color} />
-          ),
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          tabBarButton: HapticTab,
+          tabBarBackground: TabBarBackground,
+          tabBarStyle: Platform.select({
+            ios: {
+              // Use a transparent background on iOS to show the blur effect
+              position: "absolute",
+            },
+            default: {
+              backgroundColor: "black",
+            },
+          }),
         }}
-      />
-      <Tabs.Screen
-        name="(mission)"
-        options={{
-          title: t("mission"),
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="list-alt" size={28} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="(career)"
-        options={{
-          title: t("career"),
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons
-              name="insert-chart-outlined"
-              size={28}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="(inventory)"
-        options={{
-          title: t("inventory"),
-          popToTopOnBlur: true,
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="inventory" size={28} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="setting"
-        options={{
-          title: t("settings.name"),
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="settings" size={28} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="(store)"
+          options={{
+            title: t("store"),
+            popToTopOnBlur: true,
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="storefront" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(mission)"
+          options={{
+            title: t("mission"),
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="list-alt" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(career)"
+          options={{
+            title: t("career"),
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons
+                name="insert-chart-outlined"
+                size={28}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(inventory)"
+          options={{
+            title: t("inventory"),
+            popToTopOnBlur: true,
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="inventory" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="setting"
+          options={{
+            title: t("settings.name"),
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="settings" size={28} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+      {Platform.OS === "android" && <UpdatePopup />}
+    </Fragment>
   );
 }
